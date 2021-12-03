bb7d6a735cb84b00da6057648cbbc56b39ceace8
Jono <j.a.k.jarvis@gmail.com>
2021-12-03 17:15:36 +0100

    Update solutions21.py

--- advent/solutions21.py
+++ advent/solutions21.py
@@ -16,6 +16,37 @@ def day1_2021(part1_input):
 
     return part1_solution, part2_solution
 
+#Day 2
+def day2_2021(part1_input):
+    input_list = part1_input.split(' ')
+    depth = 0
+    distance = 0
+    for direction in range(0, len(input_list), 2):
+        if input_list[direction] == 'forward':
+            distance += int(input_list[direction + 1])
+        elif input_list[direction] == 'down':
+            depth += int(input_list[direction + 1])
+        elif input_list[direction] == 'up':
+            depth -= int(input_list[direction + 1])
+        else:
+            print('Incorrect direction entered')
+    part1_solution = depth * distance
+
+    aim = 0
+    depth = 0
+    distance = 0
+    for direction in range(0, len(input_list), 2):
+        if input_list[direction] == 'forward':
+            distance += int(input_list[direction + 1])
+            depth += aim * int(input_list[direction + 1])
+        elif input_list[direction] == 'down':
+            aim += int(input_list[direction + 1])
+        elif input_list[direction] == 'up':
+            aim -= int(input_list[direction + 1])
+        else:
+            print('Incorrect direction entered')
+    part1_solution = depth * distance
+
 def error(part1_input):
     part1_solution = "Sorry, this puzzle hasn't been solved yet!"
     part2_solution = "Sorry, this puzzle hasn't been solved yet!"
