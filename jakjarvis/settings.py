ef3136deacf6ca128c3874ff91d06fd844f5f50f
Jono Jarvis <j.a.k.jarvis@gmail.com>
2022-11-30 17:28:36 +0100

    created secrets file

--- jakjarvis/settings.py
+++ jakjarvis/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import jakjarvis.secrets as secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r53qtvn=ncu+=$@5o(5qx!d=s0a$esq^=lp(#!2j4v_e9((rz="
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
