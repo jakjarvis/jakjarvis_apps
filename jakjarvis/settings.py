b8fe4000cda1d4e12b7a653f2c3cb87e60d91dc8
Jono Jarvis <j.a.k.jarvis@gmail.com>
2023-03-01 12:17:12 +0100

    updated settings for new subdomain

--- jakjarvis/settings.py
+++ jakjarvis/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["jakjarvis.pythonanywhere.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["apps.jakjarvis.com", "127.0.0.1"]
 
 
 # Application definition
